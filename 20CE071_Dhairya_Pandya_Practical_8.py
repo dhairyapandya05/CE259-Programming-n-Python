cd2cc300e083530c68d2e46f0750e90e7e1b8867
dhapandya <97798059+dhapandya@users.noreply.github.com>
2022-03-16 23:51:08 +0530

    prac 8 and 9

---
+++ 20CE071_Dhairya_Pandya_Practical_8.py
@@ -0,0 +1,16 @@
+STACK = []
+
+STACK.append('A')
+STACK.append('B')
+STACK.append('C')
+
+print('INITIAL STACK')
+print(STACK)
+
+print('\nELEMENTS POPPED FROM STACK:')
+print(STACK.pop())
+print(STACK.pop())
+print(STACK.pop())
+
+print('\nSTACK AFTER ELEMENTS ARE POPPED:')
+print(STACK)
